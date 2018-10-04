41627cb18c5cf6085d3275f1ea24d79f3db1826c
Jessica Peters <jessicapeters@Jessicas-MacBook-Air.local>
2018-10-03 20:09:54 -0500

    calculator layout

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Jessica Peters on 9/19/18.
+//  Copyright © 2018 Jessica Peters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
